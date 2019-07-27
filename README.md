337891b3b34946483ca09e2fa839e6f60c5fe17f
ylm <87420752@qq.com>
2019-07-27 11:18:17 +0800

    add Linux include/

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # source-code
 存放Linux库文件、一些书籍的源代码
+
+# ChangLog
+- 2019/7/27 添加 Linux 库文件
---
+++ linux/include/linux/version.h
@@ -0,0 +1,8 @@
+#define LINUX_VERSION_CODE 199168
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
+#define RHEL_MAJOR 7
+#define RHEL_MINOR 6
+#define RHEL_RELEASE_VERSION(a,b) (((a) << 8) + (b))
+#define RHEL_RELEASE_CODE 1798
+#define RHEL_RELEASE "957.5.1"
+#define KERNEL_HEADERS_CHECKSUM "37aafa9e1ce9776ed27088e0d7a51412eaeeea89"
